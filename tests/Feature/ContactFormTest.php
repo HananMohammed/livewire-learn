7a5cdabf972fb73dc1ca6069847ab261f368e144
7nanMohamed <hanan_mohamed@codebase-tech.com>
2024-05-06 10:59:51 +0300

    contact form testing

---
+++ tests/Feature/ContactFormTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Livewire\ContactForm;
+use Livewire\Livewire;
+
+test('contact form', function () {
+    $response = $this->get('/contact')
+        ->assertSeeLivewire('contact-form');
+
+    $response->assertStatus(200);
+});
+
+test('contact form submit', function () {
+    Livewire::test(ContactForm::class)
+    ->set('name', 'John')
+    ->set('email', 'a@a.com')
+    ->set('phone', '123456')
+    ->set('message', 'Hello')
+    ->call('submitForm')
+    ->assertStatus(200);
+
+});
